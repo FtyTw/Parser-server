582116a75077c4db5e3f9ead87f3865693bedf31
Pavel Kirov <kirov@maguru.com>
2021-11-30 17:35:37 +0200

    olx parse configs extending + parseconfigs improvements

--- browser/parseConfigs.js
+++ browser/parseConfigs.js
@@ -1,84 +1,80 @@
-const parseConfigs = [
-	{
-		//
-		title: "domria_odessa_houses",
-		url: "houses",
-		config: "odessa",
-	},
-	{
-		//
-		title: "domria_odessa_flats",
-		url: "flats",
-		config: "odessa",
-	},
-	{
-		title: "domria_odessa_appartaments",
-		url: "appartaments",
-		config: "odessa",
-	},
-	{
-		title: "domria_kominternovo_houses",
-		url: "houses",
-		config: "kominternovo",
-	},
-	{
-		title: "domria_kominternovo_appartaments",
-		url: "appartaments",
-		config: "kominternovo",
-	},
-	{
-		title: "domria_kominternovo_flats",
-		url: "flats",
-		config: "kominternovo",
-	},
+const Types = [
+	{ type: "kvartiry/prodazha-kvartir", title: "appartaments" },
+	{ type: "doma/prodazha-domov", title: "houses" },
+	{ type: "komnaty/prodazha-komnat", title: "flats" },
+];
+const olx_config = {
+	mainSelector: ".content",
+	subSelector: ".offer:not(.promoted) .title-cell",
+};
+const besplatka_config = {
+	mainSelector: ".messages-list",
+	subSelector: ".msg-inner:not(.top) .d-title",
+};
+const olx_places = [
+	//
+	"odessa",
+	"kryzhanovka_43791",
+	"leski_43795",
+	"fontanka",
+	"shevchenko_44347",
+	"vapnyarka_43741",
+	"gvardeyskoe_43751",
+	"korsuntsy",
+	"krasnoselka_43787",
+	"novaya-dofinovka",
+	"aleksandrovka_43819",
+	"pervomayskoe_43827",
+	"sverdlovo_43841",
+];
 
-	{
-		title: "olx_odessa_appartaments",
-		url: "https://www.olx.ua/nedvizhimost/kvartiry/prodazha-kvartir/odessa/?search%5Bprivate_business%5D=private&search%5Bdistrict_id%5D=91&currency=USD",
-		config: {
-			mainSelector: ".content",
-			subSelector: ".title-cell",
-		},
-	},
-	{
-		title: "olx_odessa_houses",
-		url: "https://www.olx.ua/nedvizhimost/doma/prodazha-domov/odessa/?search%5Bprivate_business%5D=private&search%5Bdistrict_id%5D=91&currency=USD",
-		config: {
-			mainSelector: ".content",
-			subSelector: ".title-cell",
-		},
-	},
-	{
-		title: "olx_odessa_flats",
-		url: "https://www.olx.ua/nedvizhimost/komnaty/prodazha-komnat/odessa/?search%5Bprivate_business%5D=private&search%5Bdistrict_id%5D=91&currency=USD",
-		config: {
-			mainSelector: ".content",
-			subSelector: ".title-cell",
-		},
-	},
-	{
+const olxUrl = (place, type) =>
+	encodeURI(
+		`https://www.olx.ua/nedvizhimost/${type}/${place}/?search[private_business]=private${
+			place.includes("odessa") ? "&search[district_id]=91" : ""
+		}&currency=USD`
+	);
+
+const olxConfigs = olx_places
+	.map((place) => {
+		const [simplePlace] = place.split("_");
+		return Types.map(({ type, title }) => {
+			const url = olxUrl(place, type);
+
+			return {
+				title: `olx_${simplePlace}_${title}`,
+				url: olxUrl(place, type),
+				config: olx_config,
+			};
+		});
+	})
+	.flat();
+
+const domriaPlaces = ["odessa", "kominternovo"];
+
+const domriaConfigs = domriaPlaces
+	.map((place) =>
+		Types.map(({ title }) => ({
+			title: `domria_${place}_${title}`,
+			url: title,
+			config: place,
+		}))
+	)
+	.flat();
+
+const besplatkaConfigs = Types.map(({ type }) => {
+	const [, hardType] = type.split("/");
+	return {
 		title: "besplatka_odessa_appartaments",
-		url: "https://besplatka.ua/ru/odessa/nedvizhimost/prodazha-kvartir/suvorovskii-r-n/ot-sobstvennika",
-		config: {
-			mainSelector: ".messages-list",
-			subSelector: ".msg-inner:not(.top) .d-title",
-		},
-	},
-	{
-		title: "besplatka_odessa_houses",
-		url: "https://besplatka.ua/ru/odessa/nedvizhimost/prodazha-domov/suvorovskii-r-n/ot-sobstvennika",
-		config: {
-			mainSelector: ".messages-list",
-			subSelector: ".msg-inner:not(.top) .d-title",
-		},
-	},
-	{
-		title: "besplatka_odessa_flats",
-		url: "https://besplatka.ua/ru/odessa/nedvizhimost/prodazha-komnat/suvorovskii-r-n/ot-sobstvennika",
-		config: {
-			mainSelector: ".messages-list",
-			subSelector: ".msg-inner:not(.top) .d-title",
-		},
-	},
+		url: `https://besplatka.ua/ru/odessa/nedvizhimost/${hardType}/suvorovskii-r-n/ot-sobstvennika`,
+		config: besplatka_config,
+	};
+});
+
+const parseConfigs = [
+	//
+	...olxConfigs,
+	...domriaConfigs,
+	...besplatkaConfigs,
 ];
 module.exports = parseConfigs;
--- cron/index.js
+++ cron/index.js
@@ -2,7 +2,7 @@ const cron = require("node-cron");
 const browser = require("../browser");
 
 const enable = () => {
-	const interval = 5;
+	const interval = 2;
 	const when = () => new Date().toISOString();
 	console.log(`cron enabled at ${when()}`);
 	cron.schedule(`*/${interval} * * * *`, () => {
