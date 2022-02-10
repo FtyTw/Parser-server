a33fc72709f48a8de2c513e12f4f8587f0dc29e5
Pavel Kirov <kirov@maguru.com>
2022-02-10 15:12:12 +0200

    a little changes in browser initiation

--- browser/browser.js
+++ browser/browser.js
@@ -1,20 +1,27 @@
 const puppeteer = require("puppeteer");
 const shelljs = require("shelljs");
+
 async function startBrowser(parserCounter) {
     try {
         console.log("Opening the browser......");
-        const browser = await puppeteer.launch({
-            executablePath: "/usr/bin/chromium-browser",
+
+        const configs = {
             headless: true,
-            args: ["--disable-setuid-sandbox","--disable-dev-shm-usage"],
+            args: ["--disable-setuid-sandbox", "--disable-dev-shm-usage"],
             ignoreHTTPSErrors: true,
             timeout: 1000,
-        });
+        };
+
+        if (process.env.NODE_ENV !== "development") {
+            configs.executablePath = "/usr/bin/chromium-browser";
+        }
+
+        const browser = await puppeteer.launch(configs);
         return browser;
     } catch (err) {
         shelljs.exec(`pkill -9 chrome`);
         shelljs.exec(
-            `COUNTER=${parserCounter} pm2 restart server --update-env`
+            `COUNTER=${parserCounter - 1} pm2 restart server --update-env`
         );
         console.log("Could not create a browser instance => : ", err);
     }
